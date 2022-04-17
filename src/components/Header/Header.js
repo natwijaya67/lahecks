21d587f842a5cfea70c762e59a154655d34ac417
gisellenkurnia1 <gisellekurnia1@g.ucla.edu>
2022-04-16 21:03:25 -0700

    new change to header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import React from "react";
 
 export default function Header() {
 
   const displayDesktop = () => {
-    return <Toolbar>Hello App</Toolbar>;
+    return <Toolbar>{curbLogo}</Toolbar>;
   };
+
+  const curbLogo = (
+      <Typography variant = "h6" component="h1">
+          curb
+      </Typography>
+  );
 
 
   return (
