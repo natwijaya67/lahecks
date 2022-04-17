369b47c94148346e99f0e4babf81d12dbbd942df
gisellenkurnia1 <gisellekurnia1@g.ucla.edu>
2022-04-16 21:14:45 -0700

    change to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,27 @@
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, makeStyles } from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles(() => ({
+    header: {
+        backgroundColor: "#400CCC", 
+    },
+    logo : {
+        fontFamily: "Work Sans, sans-serif", 
+        fontWeight: 600, 
+        color: "#FFFEEFE", 
+        textAlign: "left",
+    },
+}));
+
 export default function Header() {
+
+  const { header } = useStyles();
 
   const displayDesktop = () => {
     return <Toolbar>{curbLogo}</Toolbar>;
   };
+
+  
 
   const curbLogo = (
       <Typography variant = "h6" component="h1">
@@ -16,7 +32,7 @@ export default function Header() {
 
   return (
     <header>
-      <AppBar>{displayDesktop()}</AppBar>
+      <AppBar className={header}>{displayDesktop()}</AppBar>
     </header>
   );
 }
