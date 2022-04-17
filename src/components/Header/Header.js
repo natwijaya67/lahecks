91dba63a5b52b2aac95965f8eb8cd5374ed7ce1f
gisellenkurnia1 <gisellekurnia1@g.ucla.edu>
2022-04-16 21:54:20 -0700

    added heading data

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,45 @@
-import { AppBar, Toolbar, Typography, makeStyles } from "@material-ui/core";
+import { 
+    AppBar, 
+    Toolbar, 
+    Typography, 
+    makeStyles,
+    Button,
+ } from "@material-ui/core";
 import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+
+const headersData = [
+    {
+        label: "Home", 
+        href: "/home", 
+    },
+    {
+        label: "About",
+        href: "/about",
+    },
+    {
+        label: "Buy",
+        href: "/buy",
+    },
+    {
+        label: "Sell",
+        href: "/sell",
+    },
+    {
+        label: "Forum",
+        href: "/forum",
+    },
+    {
+        label: "Map",
+        href: "/map"
+    },
+];
 
 const useStyles = makeStyles(() => ({
     header: {
         backgroundColor: "#400CCC", 
+        paddingRight: "79px",
+        paddingLeft: "118px",
     },
     logo : {
         fontFamily: "Work Sans, sans-serif", 
@@ -11,23 +47,50 @@ const useStyles = makeStyles(() => ({
         color: "#FFFEEFE", 
         textAlign: "left",
     },
+    menuButton: {
+        fontFamily: "Open Sans, sans-serif",
+        fontWeight: 700,
+        size: "18px",
+        marginLeft: "38px",
+    },
 }));
 
 export default function Header() {
 
-  const { header } = useStyles();
+  const { header, logo } = useStyles();
 
   const displayDesktop = () => {
-    return <Toolbar>{curbLogo}</Toolbar>;
+    return (
+    <Toolbar>
+        {curbLogo}
+        {getMenuButtons()}
+        </Toolbar>
+    );
   };
 
-  
-
   const curbLogo = (
-      <Typography variant = "h6" component="h1">
+      <Typography variant = "h6" component="h1" className ={logo}>
           curb
       </Typography>
   );
+
+  const getMenuButtons = () => {
+      return headersData.map(({ label, href }) => {
+          return (
+              <Button
+            {
+            ...
+            {
+                key: label, 
+                color: "inherit",
+                to: href,
+                component: RouterLink,
+            } }
+            >  {label}
+            </Button>
+          );
+        });
+    };
 
 
   return (
